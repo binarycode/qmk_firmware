df1001a063f2635cf0563a80e53f4cf60dd1298c
Igor Sidorov <igor.sidorov@onapp.com>
2019-04-04 17:25:18 +0300

    moved tab a bit closer

--- keyboards/mitosis/keymaps/binarycode/keymap.c
+++ keyboards/mitosis/keymaps/binarycode/keymap.c
@@ -27,17 +27,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |--------+--------+--------+--------+--------||--------+--------+--------+--------+--------|
    * | Z      | X      | C      | V      | B      || N      | M      | ,      | .      | /      |
    * '--------+--------+--------+--------+--------||--------+--------+--------+--------+--------'
-   *          |        | LCTRL  | ESCAPE | FN     || NUM    | BACKSP | RCTRL  | TAB    |
+   *          |        | LCTRL  | ESCAPE | FN     || NUM    | BACKSP | TAB    |        |
    *          |--------+--------+--------+--------||--------+--------+--------+--------|
-   *          | LALT   | LGUI   | SPACE  | LSHIFT || RSHIFT | ENTER  | RCTRL  | RALT   |
+   *          | LGUI   | LALT   | SPACE  | LSHIFT || RSHIFT | ENTER  | RALT   |        |
    *          '-----------------------------------''-----------------------------------'
    */
   [BASE] = LAYOUT(
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,           KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,           KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-             XXXXXXX, KC_LCTL, KC_ESC,  MO(FN),         MO(NUM), KC_BSPC, KC_RCTL, KC_TAB,
-             KC_LALT, KC_LGUI, KC_SPC,  KC_LSFT,        KC_RSFT, KC_ENT,  KC_RCTL, KC_RALT
+             XXXXXXX, KC_LCTL, KC_ESC,  MO(FN),         MO(NUM), KC_BSPC, KC_TAB,  XXXXXXX,
+             KC_LGUI, KC_LALT, KC_SPC,  KC_LSFT,        KC_RSFT, KC_ENT,  KC_RALT, XXXXXXX
   ),
 
   /* NUMBER LAYER
