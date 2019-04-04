9b40def26200209bb66e456e1b2fcb57c13173e2
Igor Sidorov <igor.sidorov@onapp.com>
2019-04-04 11:22:33 +0300

    add GUI layer, remove unused keys

--- keyboards/mitosis/keymaps/binarycode/keymap.c
+++ keyboards/mitosis/keymaps/binarycode/keymap.c
@@ -12,9 +12,12 @@
 #define BASE 0
 #define NUM  1
 #define FN   2
+#define GUI  3
 
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
+
+#define KC_TMUX LCTL(KC_A)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -26,22 +29,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |--------+--------+--------+--------+--------||--------+--------+--------+--------+--------|
    * | Z      | X      | C      | V      | B      || N      | M      | ,      | .      | /      |
    * '--------+--------+--------+--------+--------||--------+--------+--------+--------+--------'
-   *          |        |        | LCTRL  | SPACE  || LSHIFT |        |        | BACKSP |
+   *          | TMUX   | LCTRL  | ESCAPE | FN     || NUM    | BACKSP | RCTRL  | TAB    |
    *          |--------+--------+--------+--------||--------+--------+--------+--------|
-   *          |        | LGUI   | ESCAPE | FN     || NUM    | ENTER  |        |        |
+   *          | LALT   | LGUI   | SPACE  | LSHIFT || RSHIFT | ENTER  | RCTRL  | RALT   |
    *          '-----------------------------------''-----------------------------------'
    */
   [BASE] = LAYOUT(
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,           KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,           KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-             XXXXXXX, XXXXXXX, KC_LCTL, KC_SPC,         KC_LSFT, XXXXXXX, XXXXXXX, KC_BSPC,
-             XXXXXXX, KC_LGUI, KC_ESC,  MO(FN),         MO(NUM), KC_ENT,  XXXXXXX, XXXXXXX
+             KC_TMUX, KC_LCTL, KC_ESC,  MO(FN),         MO(NUM), KC_BSPC, KC_RCTL, KC_TAB,
+             KC_LALT, KC_LGUI, KC_SPC,  KC_LSFT,        KC_RSFT, KC_ENT,  KC_RCTL, KC_RALT
   ),
 
   /* NUMBER LAYER
    * .--------------------------------------------..--------------------------------------------.
-   * | TAB    |        |        | -      | =      || ]      | pad *  | pad +  | pad -  | [      |
+   * |        |        |        | -      | =      || ]      |        |        |        | [      |
    * |--------+--------+--------+--------+--------||--------+--------+--------+--------+--------|
    * | 1      | 2      | 3      | 4      | 5      || 6      | 7      | 8      | 9      | 0      |
    * |--------+--------+--------+--------+--------||--------+--------+--------+--------+--------|
@@ -53,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *          '-----------------------------------''-----------------------------------'
    */
   [NUM] = LAYOUT(
-    KC_TAB,  _______, _______, KC_MINS, KC_EQL,        KC_RBRC, KC_PAST, KC_PPLS, KC_PMNS, KC_LBRC,
+    _______, _______, _______, KC_MINS, KC_EQL,        KC_RBRC, _______, _______, _______, KC_LBRC,
     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,         KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
              KC_F11,  KC_F12,  _______, _______,       _______, _______, _______, _______,
@@ -63,23 +66,45 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* FN LAYER
    * .--------------------------------------------..--------------------------------------------.
-   * | `      |        | MS_U   |        |        || WH_U   | WH_L   | BTN3   | WH_R   | [      |
+   * | `      |        |        |        |        ||        |        |        |        |        |
    * |--------+--------+--------+--------+--------||--------+--------+--------+--------+--------|
-   * |        | MS_L   | MS_D   | MS_R   |        || WH_D   | BTN1   | BTN2   |        | '      |
+   * |        |        |        |        |        || LEFT   | DOWN   | UP     | RIGHT  | '      |
    * |--------+--------+--------+--------+--------||--------+--------+--------+--------+--------|
-   * | APP    | MPRV   | MPLY   | MSTP   | MNXT   ||        | BSPC   | DEL    | INS    | \      |
+   * |        |        |        |        |        ||        |        |        |        | \      |
    * '--------+--------+--------+--------+--------||--------+--------+--------+--------+--------'
-   *          | VOLU   |        |        |        ||        |        | PGUP   |        |
+   *          |        |        |        |        ||        |        |        |        |
    *          |--------+--------+--------+--------||--------+--------+--------+--------|
-   *          | VOLD   |        |        |        ||        | HOME   | PGDN   | END    |
+   *          |        |        |        |        ||        |        |        |        |
    *          '-----------------------------------''-----------------------------------'
    */
   [FN] = LAYOUT(
-    KC_GRV,  _______, KC_MS_U, _______, _______,       KC_WH_U, KC_WH_L, KC_BTN3, KC_WH_R, KC_LBRC,
-    _______, KC_MS_L, KC_MS_D, KC_MS_R, _______,       KC_WH_D, KC_BTN1, KC_BTN2, _______, KC_QUOT,
-    KC_APP,  KC_MPRV, KC_MPLY, KC_MSTP, KC_MNXT,       _______, KC_BSPC, KC_DEL,  KC_INS,  KC_BSLS,
-             KC_VOLU, _______, _______, _______,       _______, _______, KC_PGUP, _______,
-             KC_VOLD, _______, _______, _______,       _______, KC_HOME, KC_PGDN, KC_END
+    KC_GRV,  _______, _______, _______, _______,       _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______,       KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT,
+    _______, _______, _______, _______, _______,       _______, _______, _______, _______, KC_BSLS,
+             _______, _______, _______, _______,       _______, _______, _______, TG(GUI),
+             _______, _______, _______, _______,       _______, _______, _______, _______
+  ),
+
+
+  /* GUI LAYER
+   * .--------------------------------------------..--------------------------------------------.
+   * |        |        |        |        |        ||        |        |        |        |        |
+   * |--------+--------+--------+--------+--------||--------+--------+--------+--------+--------|
+   * | MEDUIM | FAST   | BTN2   | BTN1   |        || MS_L   | MS_D   | MS_U   | MS_R   |        |
+   * |--------+--------+--------+--------+--------||--------+--------+--------+--------+--------|
+   * |        |        |        |        |        ||        |        |        |        |        |
+   * '--------+--------+--------+--------+--------||--------+--------+--------+--------+--------'
+   *          |        |        |        |        ||        |        |        | BASE   |
+   *          |--------+--------+--------+--------||--------+--------+--------+--------|
+   *          |        |        |        |        ||        |        |        |        |
+   *          '-----------------------------------''-----------------------------------'
+   */
+  [GUI] = LAYOUT(
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    KC_ACL0, KC_ACL2, KC_BTN2, KC_BTN1, XXXXXXX,       KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, TG(GUI),
+             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
   ),
 };
 
@@ -95,6 +120,9 @@ void matrix_scan_user(void) {
         break;
       case NUM:
         set_led_red;
+        break;
+      case GUI:
+        set_led_green;
         break;
       default:
         break;
