8cc81b434ebe1d295c669d31fd9abad89bda0b46
Igor Sidorov <igor.sidorov@onapp.com>
2020-11-23 14:07:38 +0300

    reworked layout

--- keyboards/mitosis/keymaps/binarycode/TODO.md
+++ keyboards/mitosis/keymaps/binarycode/TODO.md
@@ -1,17 +1,11 @@
 * README.md
-* WORKSPACE UP/DOWN above arrow keys
-* separate layout for FUNCTION to have FXX keys? one-shot maybe? maybe short for number, long for FXX?
-* a lot of HOLD/TAP/DOUBLE HOLD/DOUBLE TAP dances everywhere
-* investigate `register_code16`/`register_mods`
-* move more keys to thumb space - maybe -,+,=,or brackets?
-* blinking on mouse lock??
-* do we need diagonal mouse movement?
 * double-tap LEADER like in `keyboards/ergodox_ez/keymaps/hacker_dvorak/tap_dance/tap_dances.c`
-* brackets and curly bracket on macro layer index and middle fingers?
 * maybe app switching should be on both hands, but different directions?
-* media keys
 * one-shot layer modifiers?
-* try hjkl mouse keys
+
+# longterm
+* media keys
+* blinking on mouse lock??
 
 # Interesting keymaps
 * `users/gordon/gordon.c`
--- keyboards/mitosis/keymaps/binarycode/keymap.c
+++ keyboards/mitosis/keymaps/binarycode/keymap.c
@@ -43,26 +43,6 @@ enum {
     KC_INFO,
     // tmux copy mode
     KC_SCROL,
-    // tmux
-    KC_CTRLA,
-    KC_CTRLB
-};
-
-// Tap dances
-enum {
-    TD_L_LOWER,
-    TD_R_LOWER
-};
-
-// Tap dance states
-enum {
-    UNKNOWN,
-    SINGLE_TAP,
-    SINGLE_HOLD,
-    DOUBLE_TAP,
-    DOUBLE_HOLD,
-    TRIPLE_TAP,
-    TRIPLE_HOLD
 };
 
 // For consistency with diagonal mouse keys
@@ -73,14 +53,8 @@ enum {
 
 // Toggle layout
 #define KC_MOUSE TG(MOUSE)
-
-// Tap dance
-#define KC_L_LWR TD(TD_L_LOWER)
-#define KC_R_LWR TD(TD_R_LOWER)
-
-// Copy-paste
-#define KC_CTRLC LCTL(KC_C)
-#define KC_CTRLV LCTL(KC_V)
+#define KC_L_LWR LT(NAVIGATION, KC_ESC)
+#define KC_R_LWR LT(NUMPAD, KC_ENT)
 
 // Special functions
 #define KC_MNVIM LCTL(KC_F7)
@@ -89,16 +63,24 @@ enum {
 #define KC_TMUX2 LCTL(KC_F2)
 #define KC_TMUX3 LCTL(KC_F3)
 
+// Switch windows
+#define KC_SWTCH LALT(KC_ESC)
+
+// Switch workspaces
+#define KC_WSPC1 LGUI(KC_1)
+#define KC_WSPC2 LGUI(KC_2)
+#define KC_WSPC3 LGUI(KC_3)
+
 #define KC_    KC_TRNS
 #define KC_XXX KC_NO
-
-#define MOD_MASK_SHIFT (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT))
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* BASE LAYER
- * HOLD NAV for NAVIGATION layer, TAP NAV for KC_TAB
- * HOLD NUM for NUMPAD layer, TAP NUM for KC_UNDR (_)
+ * HOLD L_LWR for NAVIGATION layer, TAP L_LWR for KC_ESC
+ * HOLD R_LWR for NUMPAD layer, TAP R_LWR for KC_ENT
+ * TODO: double shift for new layer
+ * TODO: L_LWR+RSFT, R_LWR+LSFT
  */
 [BASE] = LAYOUT_kc(
 //  ┌───────┬───────┬───────┬───────┬───────┐    ┌───────┬───────┬───────┬───────┬───────┐
@@ -108,30 +90,34 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //  ├───────┼───────┼───────┼───────┼───────┤    ├───────┼───────┼───────┼───────┼───────┤
         Z   ,   X   ,   C   ,   V   ,   B   ,        N   ,   M   , COMM  ,  DOT  , SLSH  ,
 //  └───────┼───────┼───────┼───────┼───────┤    ├───────┼───────┼───────┼───────┼───────┘
-              CTRLC , LCTL  ,  ESC  , L_LWR ,      R_LWR , BSPC  , RCTL  , CTRLV ,
+                    ,       ,  SPC  , L_LWR ,      R_LWR ,  TAB  ,       ,       ,
 //          ├───────┼───────┼───────┼───────┤    ├───────┼───────┼───────┼───────┤
-              CTRLA , LALT  ,  SPC  , LSFT  ,      RSFT  ,  ENT  , RALT  , LGUI
+              LGUI  , LALT  , LCTL  , LSFT  ,      RSFT  , RCTL  , RALT  , RGUI
 //          └───────┴───────┴───────┴───────┘    └───────┴───────┴───────┴───────┘
 ),
 
 /* NUMPAD LAYER
+ * TODO: 2 unassigned keys
+ * TODO: what can we do on the right side???
  */
 [NUMPAD] = LAYOUT_kc(
 //  ┌───────┬───────┬───────┬───────┬───────┐    ┌───────┬───────┬───────┬───────┬───────┐
-       GRV  ,   7   ,   8   ,   9   , MINS  ,      RBRC  , RPRN  , RCBR  ,       ,       ,
+       GRV  ,   7   ,   8   ,   9   , MINS  ,            ,       ,       ,       ,       ,
 //  ├───────┼───────┼───────┼───────┼───────┤    ├───────┼───────┼───────┼───────┼───────┤
-       XXX  ,   4   ,   5   ,   6   ,  EQL  ,      LBRC  , LPRN  , LCBR  ,       ,       ,
+       XXX  ,   4   ,   5   ,   6   ,  EQL  ,            ,       ,       ,       ,       ,
 //  ├───────┼───────┼───────┼───────┼───────┤    ├───────┼───────┼───────┼───────┼───────┤
-       XXX  ,   1   ,   2   ,   3   ,   0   ,       TAB  , UNDS  ,       ,       ,       ,
+       XXX  ,   1   ,   2   ,   3   ,   0   ,            ,       ,       ,       ,       ,
 //  └───────┼───────┼───────┼───────┼───────┤    ├───────┼───────┼───────┼───────┼───────┘
-                    ,       ,       ,       ,            ,       ,       ,       ,
+                    ,       , BSPC  ,       ,            ,       ,       ,       ,
 //          ├───────┼───────┼───────┼───────┤    ├───────┼───────┼───────┼───────┤
-                    ,       ,       ,  TAB  ,            ,       ,       ,
+                    , LCBR  , LBRC  , LPRN  ,            ,       ,       ,
 //          └───────┴───────┴───────┴───────┘    └───────┴───────┴───────┴───────┘
 ),
 
 /* NAVIGATION LAYER
  * contains keys that did not fit on BASE layer ([, ], \, ')
+ * TODO: 3 unassigned keys
+ * TODO: not sure if this is a right place for DEL
  */
 [NAVIGATION] = LAYOUT_kc(
 //  ┌───────┬───────┬───────┬───────┬───────┐    ┌───────┬───────┬───────┬───────┬───────┐
@@ -141,9 +127,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //  ├───────┼───────┼───────┼───────┼───────┤    ├───────┼───────┼───────┼───────┼───────┤
             ,  F1   ,  F2   ,  F3   ,  F10  ,      HOME  , PGDN  , PGUP  , END   ,  XXX  ,
 //  └───────┼───────┼───────┼───────┼───────┤    ├───────┼───────┼───────┼───────┼───────┘
-                    ,       ,       ,       ,            ,  DEL  ,       ,       ,
+                    ,       ,  DEL  ,       ,            ,       ,       ,       ,
 //          ├───────┼───────┼───────┼───────┤    ├───────┼───────┼───────┼───────┤
-                    ,       ,       ,       ,      UNDS  ,       ,       ,
+                    ,       ,       ,       ,      RPRN  , RBRC  , RCBR  ,
 //          └───────┴───────┴───────┴───────┘    └───────┴───────┴───────┴───────┘
 ),
 
@@ -152,11 +138,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [MACRO] = LAYOUT_kc(
 //  ┌───────┬───────┬───────┬───────┬───────┐    ┌───────┬───────┬───────┬───────┬───────┐
-       XXX  ,  XXX  ,  XXX  ,  XXX  ,  XXX  ,      PSCR  ,  XXX  ,  XXX  ,  XXX  ,  XXX  ,
+       XXX  ,  XXX  ,  XXX  ,  XXX  ,  XXX  ,      WSPC1 ,  XXX  ,  XXX  ,  XXX  , PSCR  ,
 //  ├───────┼───────┼───────┼───────┼───────┤    ├───────┼───────┼───────┼───────┼───────┤
-       XXX  ,  XXX  , MNVIM , FINGR , SCROL ,       XXX  ,  XXX  , MOUSE ,  XXX  ,  XXX  ,
+       XXX  ,  XXX  , MNVIM , FINGR , SCROL ,      WSPC2 , SWTCH , MOUSE ,  XXX  ,  XXX  ,
 //  ├───────┼───────┼───────┼───────┼───────┤    ├───────┼───────┼───────┼───────┼───────┤
-       XXX  , TMUX1 , TMUX2 , TMUX3 ,  XXX  ,       XXX  ,  XXX  ,  XXX  ,  XXX  , INFO  ,
+       XXX  , TMUX1 , TMUX2 , TMUX3 ,  XXX  ,      WSPC3 ,  XXX  ,  XXX  ,  XXX  , INFO  ,
 //  └───────┼───────┼───────┼───────┼───────┤    ├───────┼───────┼───────┼───────┼───────┘
                     ,       ,       ,       ,            ,       ,       ,       ,
 //          ├───────┼───────┼───────┼───────┤    ├───────┼───────┼───────┼───────┤
@@ -224,34 +210,6 @@ inline bool tmux_copy_mode(bool pressed) {
     return false;
 }
 
-inline bool tmux_slave(bool pressed) {
-    if (pressed) {
-        register_code(KC_LCTL);
-        tap_code(KC_A);
-        /*register_code(KC_A);*/
-        /*SEND_STRING(SS_DOWN(X_LCTL) SS_DOWN(X_A));*/
-    } else {
-        /*unregister_code(KC_A);*/
-        unregister_code(KC_LCTL);
-        /*SEND_STRING(SS_UP(X_A) SS_UP(X_LCTL));*/
-    }
-    return false;
-}
-
-inline bool tmux_master(bool pressed) {
-    if (pressed) {
-        register_code(KC_LCTL);
-        tap_code(KC_B);
-        /*register_code(KC_B);*/
-        /*SEND_STRING(SS_DOWN(X_LCTL) SS_DOWN(X_B));*/
-    } else {
-        /*unregister_code(KC_B);*/
-        unregister_code(KC_LCTL);
-        /*SEND_STRING(SS_UP(X_B) SS_UP(X_LCTL));*/
-    }
-    return false;
-}
-
 uint32_t layer_state_set_user(uint32_t state) {
     toggle_mouse_lock(false);
 
@@ -306,12 +264,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
         case KC_SCROL:
             return tmux_copy_mode(pressed);
-
-        case KC_CTRLA:
-            return tmux_slave(pressed);
-
-        case KC_CTRLB:
-            return tmux_master(pressed);
     }
     return true;
 }
@@ -319,82 +271,5 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 void keyboard_post_init_user() {
     set_led_off;
 }
-
-int cur_dance(qk_tap_dance_state_t *state) {
-    if (state->count == 1) {
-        if (state->pressed) return SINGLE_HOLD;
-        else return SINGLE_TAP;
-    }
-    if (state->count == 2) {
-        if (state->pressed) return DOUBLE_HOLD;
-        else return DOUBLE_TAP;
-    }
-    if (state->count == 3) {
-        if (state->pressed) return TRIPLE_HOLD;
-        else return TRIPLE_TAP;
-    }
-    return UNKNOWN;
-}
-
-static int l_lower_state;
-
-void l_lower_tap(qk_tap_dance_state_t *state, void *user_data) {
-    layer_on(NAVIGATION);
-}
-
-void l_lower_finished(qk_tap_dance_state_t *state, void *user_data) {
-    /*l_lower_state = cur_dance(state);*/
-    /*switch (l_lower_state) {*/
-        /*case SINGLE_TAP:*/
-            /*register_code(KC_TAB);*/
-            /*break;*/
-    /*}*/
-}
-
-void l_lower_reset(qk_tap_dance_state_t *state, void *user_data) {
-    switch (l_lower_state) {
-        /*case DOUBLE_HOLD:*/
-            /*break;*/
-
-        /*case SINGLE_TAP:*/
-            /*unregister_code(KC_TAB);*/
-            // intentionally no break to make sure the layer is off
-
-        default:
-            layer_off(NAVIGATION);
-            break;
-    }
-}
-
-static int r_lower_state;
-
-void r_lower_tap(qk_tap_dance_state_t *state, void *user_data) {
-    layer_on(NUMPAD);
-}
-
-void r_lower_finished(qk_tap_dance_state_t *state, void *user_data) {
-    /*r_lower_state = cur_dance(state);*/
-    /*switch (r_lower_state) {*/
-        /*case SINGLE_TAP:*/
-            /*SEND_STRING("_");*/
-            /*break;*/
-    /*}*/
-}
-
-void r_lower_reset(qk_tap_dance_state_t *state, void *user_data) {
-    switch (r_lower_state) {
-        /*case DOUBLE_HOLD:*/
-            /*break;*/
-
-        default:
-            layer_off(NUMPAD);
-            break;
-    }
-}
-
-qk_tap_dance_action_t tap_dance_actions[] = {
-    [TD_L_LOWER] = ACTION_TAP_DANCE_FN_ADVANCED(l_lower_tap, l_lower_finished, l_lower_reset),
-    [TD_R_LOWER] = ACTION_TAP_DANCE_FN_ADVANCED(r_lower_tap, r_lower_finished, r_lower_reset),
-};
 
 /* vim: set ts=4 sw=4 sts=4 et: */
